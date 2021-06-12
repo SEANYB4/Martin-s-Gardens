3608e59ea615d381e13f0064e55719f03242183d
Sean Bain <75031999+SeanyB4@users.noreply.github.com>
2021-06-12 06:54:33 +0100

    Create CircleImage.swift

---
+++ MapView/MapView.swift/MG Prototype 2/Created by Sean Bain on 10/06/2021./CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  MG Prototype 2
+//
+//  Created by Sean Bain on 10/06/2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("unnamed"))
+    }
+}
