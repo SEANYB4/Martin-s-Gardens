062695d36b56f9ca1d285affbbe5d5ad0899194b
Sean Bain <75031999+SeanyB4@users.noreply.github.com>
2021-06-12 06:54:59 +0100

    Create ContentView.swift

---
+++ MapView/MapView.swift/MG Prototype 2/Created by Sean Bain on 10/06/2021./ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MG Prototype 2
+//
+//  Created by Sean Bain on 10/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
